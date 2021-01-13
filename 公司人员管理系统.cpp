a204636848ebf26c2a4fd89a580224f65bfbb059
ildnyy <2114829840@qq.com>
2021-01-13 12:41:56 +0800

    fixed:Query and Change

--- 公司人员管理系统.cpp
+++ 公司人员管理系统.cpp
@@ -4,13 +4,12 @@
 #include <string.h>
 using namespace std; 
 
-//全局数据，对象
 double ManagerSalary;         //经理固定月薪
 double SalesManagerSalary;      //销售经理固定月薪
 double SalesManagerPercent;   //销售经理提成％
 double SalesPercent;          //销售人员提成％
 double WagePerHour;         //技术人员小时工资
-int ID;                     //员工标识(要保证唯一)
+int ID;                     //员工标识
 /***********************员工类**********************/
 class Person  
 {
@@ -75,7 +74,7 @@ public:
 	{
 		this->Amount=Amount;
 	}
-    double GetAmount()  //用于计算月销售总额sum
+    double GetAmount()  
 	{
 		return Amount;
 	} 
@@ -117,7 +116,7 @@ public:
 class Company  
 {
 private:
-	Person *Worker;  /*员工表*/ 
+	Person *Worker;
 	Person *sales; 
     Person *technicians;
 public:
@@ -154,12 +153,12 @@ public:
 
 void Company::Add()
 {
-    Person *p;  //新结点指针
+    Person *p;  
     int Duty;  
     char Name[20];
     double Amount,T;
 
-    cout<<"\n** 新增员工 **\n";    //输入员工信息
+    cout<<"\n** 新增员工 **\n";   
     ID++;
     cout<<"输入岗位(1-经理2-销售经理3-销售员4-技术员):";  cin>>Duty;
     cout<<"输入姓名:";  cin>>Name;
@@ -173,7 +172,6 @@ void Company::Add()
 		cin>>T;
 	}
 
-    //创建新员工结点
     switch(Duty)
 	{
 		case 1:p=new Manager(ID,Name,Duty); break;
@@ -183,20 +181,19 @@ void Company::Add()
 	}
 	p->next=0;
 
-    //员工结点加入链表
-    if(Worker)  //若已经存在结点
+    if(Worker) 
 	{
 		Person *p2;
 		p2=Worker;
-		while(p2->next)  //查找尾结点
+		while(p2->next)  
 		{
 			p2=p2->next;
 		}
-        p2->next=p;  //连接
+        p2->next=p; 
 	}
-    else  //若不存在结点(表空)
+    else 
 	{
-		Worker=p;  //连接
+		Worker=p;  
 	}
 }
 /**************************删除*************************/
@@ -206,7 +203,6 @@ void Company::Delete()  //删除人员
     cout<<"\n** 删除员工 **\n";
     cout<<"ID:";  cin>>No;
 
-    //查找要删除的结点
     Person *p1,*p2;  p1=Worker;
     while(p1)
 	{
@@ -219,22 +215,21 @@ void Company::Delete()  //删除人员
 		}
 	}
 
-    //删除结点
-    if(p1!=NULL)//若找到结点，则删除
+    if(p1!=NULL)
 	{
-		if(p1==Worker)  //若要删除的结点是第一个结点
+		if(p1==Worker)  
 		{
 			Worker=p1->next;
 			delete p1;
 		}
-        else  //若要删除的结点是后续结点
+        else  
 		{
 			p2->next=p1->next;
 			delete p1;
 		}
 		cout<<"找到并删除\n";
 	}
-    else  //未找到结点
+    else 
 		cout<<"未找到!\n";
 }
 void Company::Change()  //修改人员
@@ -244,7 +239,6 @@ void Company::Change()  //修改人员
     cout<<"\n** 修改员工信息 **\n";
     cout<<"ID:";  cin>>No;
 
-    //查找要修改的结点
     Person *p1,*p2;  p1=Worker;
     while(p1)
 	{
@@ -294,13 +288,13 @@ void Company::display()
   double sum2=0;  //工资总和
   while(p)
   {
-    if(p->Duty==2)((SalesManager *)p)->SetAmount(sum); //sum和sum2必须分开计算
+    if(p->Duty==2)((SalesManager *)p)->SetAmount(sum); 
     p->Output();
     sum2+=p->Earning;
     p=p->next;
   }
 
-  cout<<"本月盈利:"<<sum*0.40-sum2<<"  (按照40％利润计算)"<<endl;
+  cout<<"本月盈利:"<<sum*0.60-sum2<<"  (按照40％利润计算)"<<endl;
 
 }
 /***********************设置基础数据**************************/
@@ -332,7 +326,6 @@ void Company:: Query()
     if(a==1)
 	{
        cout<<"ID:";  cin>>No;
-		//查找要修改的结点
 	   while(p1)
 	   {
 			if(p1->No==No)
@@ -347,74 +340,20 @@ void Company:: Query()
 	else 
 	{
 		cout<<"姓名:";  cin>>Name;
-
-		//查找要修改的结点
-		Person *p1,*p2;  p1=Worker;
 		while(p1)
 		{
 			if((strcmp(p1->Name,Name))==0)
-			break;
+				break;
 			else
-			{	p2=p1;	p1=p1->next;  }
+			{	
+				p2=p1;	p1=p1->next;  
+			}
 		}
 	}
 	if(p1) p1->Output();
 	else cout<<"未找到对应人员"<<endl;
 }
 
-//void Company:: Bubblesort(Person *head)
-//{
-//    Person *cur = head;
-//    Person *tail = head;
-//    Person *p = head;
-//    double a,b;
-//    int length = 0;
-//    int flag = 0;
-//    while (tail != NULL)
-//    {
-//        tail = tail->next;
-//    }
-//    while (p->next!=tail)
-//    {
-//        flag = 0;
-//        cur = head;
-//        while (cur->next != tail)
-//        {
-//        	if(cur->Duty==3){
-//        		double c=((Sales *)cur)->GetAmount();
-//        	    double d=((Sales *)cur->next)->GetAmount();
-//	            if (c > d)
-//	            {
-//	            	cout<<"11";
-//	                //前后交换
-//	                a = c;
-//	                c = d;
-//	                d = a;
-//	                flag = 1;
-//	            }
-//			}
-//			else{
-//				if (cur->t > cur->next->t)
-//	            {
-//	            	cout<<"22";
-//	                //前后交换
-//	                b = cur->t;
-//	                cur->t = cur->next->t;
-//	                cur->next->t = b;
-//	                flag = 1;
-//	            }
-//			}
-//            cur = cur->next;
-//        }
-//        //尾指针向前移动
-//        tail = cur;
-//        //若为1，则证明链表已经有序
-//        if (flag == 0)
-//        {
-//            return;
-//        }
-//    }
-//}
 void Company:: Bubblesort(Person *head)//对链表进行bubble sort
 {
     Person *pre, *p,*tail;
@@ -430,7 +369,6 @@ void Company:: Bubblesort(Person *head)//对链表进行bubble sort
         	    double d=((Sales *)p->next)->GetAmount();
 	            if (c > d)
 	            {
-	            	cout<<"11";
 	                pre->next = p->next;
 	                p->next = pre->next->next;
 	                pre->next->next = p;
@@ -460,46 +398,44 @@ void Company:: Rank()
   cout<<"\n**************** 公司销售人员和技术人员本月表现排名 *********************\n";
   cout<<"编号\t姓名\t\t职位\t\t月工资\t\t销售额或工作时间"<<endl;
   Person *p=Worker;
-//  Person *sales; 
-//  Person *technicians;
   while(p)
   {
     if(p->Duty==3){
     	Person *p1;
     	p1=new Sales(p->No,p->Name,p->Duty,((Sales *)p)->GetAmount());
     	p1->next=0;
-		if(sales)  //若已经存在结点
+		if(sales)  
 		{
 			Person *p2;
 			p2=sales;
-			while(p2->next)  //查找尾结点
+			while(p2->next)  
 			{
 				p2=p2->next;
 			}
-	        p2->next=p1;  //连接
+	        p2->next=p1;  
 		}
-	    else  //若不存在结点(表空)
+	    else  
 		{
-			sales=p1;  //连接
+			sales=p1;  
 		}
 	}
 	if(p->Duty==4){
 		Person *p1;
 		p1=new Technician(p->No,p->Name,p->Duty,((Technician *)p)->GetT());
 		p1->next=0;
-		if(technicians)  //若已经存在结点
+		if(technicians)  
 		{
 			Person *p2;
 			p2=technicians;
-			while(p2->next)  //查找尾结点
+			while(p2->next) 
 			{
 				p2=p2->next;
 			}
-	        p2->next=p1;  //连接
+	        p2->next=p1; 
 		}
-	    else  //若不存在结点(表空)
+	    else  
 		{
-			technicians=p1;  //连接
+			technicians=p1; 
 		}
 	}
     p=p->next;
@@ -519,10 +455,10 @@ void Company:: Rank()
     technicians=technicians->next;
   }
 }
-/***************************保存******************************/
+
 void Company::Save()  //数据存盘(包括基础数据，人员数据),均采用文本文件
 {
-	ofstream fPerson,fBase;    //定义文件输出流对象
+	ofstream fPerson,fBase;    
 	char c;
 	cout<<"\n保存人员和基础数据,是否继续?[Y/N]:";  cin>>c;
 	if(c!='Y'&&c!='y')return;
@@ -557,32 +493,28 @@ void Company::Save()  //数据存盘(包括基础数据，人员数据),均采用文本文件
 
 void Company::Load()  //数据装入(包括基础数据，人员数据)
 {
-	//基础数据装入
-	ifstream fBase;   //建立文件输入流对象
-	char buf[80];  //buf用于保存数据文件中的注释字符串
+	ifstream fBase;   
+	char buf[80];  
 	fBase.open("base.txt",ios::in);
-	fBase>>buf>>ManagerSalary;       //经理固定月薪
-	fBase>>buf>>SalesManagerSalary;  //销售经理固定月薪
-	fBase>>buf>>SalesManagerPercent; //销售经理提成％
-	fBase>>buf>>SalesPercent;        //销售人员提成％
-	fBase>>buf>>WagePerHour;         //技术人员小时工资
-	fBase>>buf>>ID;                  //员工标识
+	fBase>>buf>>ManagerSalary;      
+	fBase>>buf>>SalesManagerSalary;  
+	fBase>>buf>>SalesManagerPercent; 
+	fBase>>buf>>SalesPercent;        
+	fBase>>buf>>WagePerHour;         
+	fBase>>buf>>ID;                 
 	fBase.close();  
  
-	//人员数据数据装入
 	ifstream fPerson;
 	Person *p=Worker;
 	int No;  char Name[20];  int Duty;
 	double Amount,T;
 	fPerson.open("person.txt",ios::in);
-	//读一条记录
 	fPerson>>No>>Name>>Duty;
 	if(Duty==3)fPerson>>Amount;
 	else if(Duty==4)fPerson>>T;
 
 	while(fPerson.good())
 	{
-		//创建员工结点
 		switch(Duty)
 		{
 			case 1:p=new Manager(No,Name,Duty);  break;
@@ -591,23 +523,20 @@ void Company::Load()  //数据装入(包括基础数据，人员数据)
 			case 4:p=new Technician(No,Name,Duty,T);  break;
 		}
 		p->next=0;
-
-		//员工结点加入链表
-		if(Worker)  //若已经存在结点
+		if(Worker)  
 		{
 			Person *p2;
 			p2=Worker;
-			while(p2->next)  //查找尾结点
+			while(p2->next)  
 			{
 				p2=p2->next;
 			}
-			p2->next=p;  //连接
+			p2->next=p;  
 		}
-		else  //若不存在结点(表空)
+		else 
 		{
-			Worker=p;  //连接
+			Worker=p;  
 		}	  
-		//读下一条记录
 		fPerson>>No>>Name>>Duty;
 		if(Duty==3)fPerson>>Amount;
 		else if(Duty==4)fPerson>>T;
